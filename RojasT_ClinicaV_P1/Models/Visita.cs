c134219e8910950c11400adbf7d8a960713d9725
Thais Rojas <thais.rojas@udla.edu.ec>
2025-05-01 14:52:01 -0500

    clase visita

---
+++ RojasT_ClinicaV_P1/Models/Visita.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RojasT_ClinicaV_P1.Models
+{
+    public class Visita
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ingrese la fecha de visita")]
+        [DataType(DataType.Date)]
+        public DateTime FechaVisita { get; set; }
+
+        [Range(0, 1000, ErrorMessage = "El valor debe ser positivo")]
+        public decimal Tarifa { get; set; }
+
+        [Required(ErrorMessage = "Ingrese el motivo de la visita")]
+        [StringLength(100)]
+        public string Motivo { get; set; }
+
+        [Display(Name = "¿Requiere medicación?")]
+        public bool RequiereMedicacion { get; set; }
+
+        
+        public int MascotaId { get; set; }
+
+        [ForeignKey("MascotaId")]
+        public Mascota Mascota { get; set; }
+    }
+}
