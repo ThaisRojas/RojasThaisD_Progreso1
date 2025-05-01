2823311068fbe912f16e6e7f641e4e6cb8b2729d
thisr <thisr@THAISBUNNY>
2025-05-01 13:39:19 -0500

    Agregar archivos de proyecto.

---
+++ RojasT_ClinicaV_P1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RojasT_ClinicaV_P1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ RojasT_ClinicaV_P1/Models/PropietarioMascota.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RojasT_ClinicaV_P1.Models
+{
+    public class PropietarioMascota
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ingrese un nombre")]
+        
+        [StringLength(20)]    
+        public string RojasT_Nombre { get; set; }
+
+        [Range(18, 65)]
+        public int Edad { get; set; }
+
+        [Range(20.00, 100.00)]
+        public decimal ValorRevision { get; set; }
+
+        public bool EsClienteFrecuente { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime FechaRegistro { get; set; }
+    }
+}
+
