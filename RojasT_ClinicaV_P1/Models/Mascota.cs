d5388cdd2e8db3335a31adb1bcd84c2d2b9bd5f5
Thais Rojas <thais.rojas@udla.edu.ec>
2025-05-01 14:14:18 -0500

    clase porpietario de la mascota con sus atributos

---
+++ RojasT_ClinicaV_P1/Models/Mascota.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RojasT_ClinicaV_P1.Models
+{
+    public class Mascota
+    {
+        public int Id { get; set; }
+
+        [StringLength(30)]
+        public string Nombre { get; set; }
+        public string Raza { get; set; }
+
+        [Range(0, 30)]
+        public int Edad { get; set; }
+        public string Sexo { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime FechaRegistro { get; set; }
+        public int PropietarioMascotaId { get; set; }
+        public PropietarioMascota Propietario { get; set; }
+    }
+}
